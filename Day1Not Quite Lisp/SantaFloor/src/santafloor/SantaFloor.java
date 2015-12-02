d1a2f3679dc150dd659eddf6dab03dff18d30372
Eude Lesperance <l.keude96@gmail.com>
2015-12-02 00:24:42 -0600

    Solved: Day 1 - Part 1

---
+++ Day1Not Quite Lisp/SantaFloor/src/santafloor/SantaFloor.java
@@ -0,0 +1,43 @@
+package santafloor;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Kinsley
+ */
+public class SantaFloor {
+    public static void part1() {
+        File file = new File("input.txt");
+        
+        Scanner sc = null;
+        try {
+            sc = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        
+        int fl = 0;
+        String in = sc.next();
+        
+        for(int i = 0; i < in.length(); i++) {
+            char c = in.charAt(i);
+            
+            if(c == '(' ) {
+                fl++;
+            } else {
+                fl--;
+            }
+        }
+        
+        System.out.println(fl);
+        sc.close();
+    }
+    
+    public static void main(String[] args) {
+        part1();
+    }
+    
+}

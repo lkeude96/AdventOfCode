934d1749be7d530aa3189e228e84a44d304af411
Eude Lesperance <l.keude96@gmail.com>
2015-12-04 15:37:53 -0600

    Solved: Day 2 - Part 2

--- Day2I Was Told There Would Be No Math/WrappingPaper/src/wrappingpaper/WrappingPaper.java
+++ Day2I Was Told There Would Be No Math/WrappingPaper/src/wrappingpaper/WrappingPaper.java
@@ -2,6 +2,7 @@ package wrappingpaper;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -34,8 +35,40 @@ public class WrappingPaper {
         sc.close();
     }
     
+    public static void part2() {
+        File file = new File("input.txt");
+        
+        Scanner sc = null;
+        try {
+            sc = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        
+        int total = 0;
+        while(sc.hasNext()){
+            String[] dimention = sc.next().split("x");
+            
+            int[] arr = {Integer.parseInt(dimention[0]),Integer.parseInt(dimention[1]),Integer.parseInt(dimention[2])};
+            Arrays.sort(arr);
+            
+            int l = arr[0];
+            int w = arr[1];
+            int h = arr[2];
+            
+            int bow = l * w * h;
+            total += (Math.min(l, w) + Math.min(w, h)) * 2 + bow;
+            System.out.printf("%dx%dx%d   min %d %d%n",l, w, h, Math.min(l, w),Math.min(w, h));
+        }
+        
+        System.out.println(total);
+        
+        sc.close();
+    }
+    
     public static void main(String[] args) {
         part1();
+        part2();
     }
     
 }
